7e8031e83f4f21f20b0bf43381bf94521009ab72
Matteo Contrini <matteo@contrini.it>
2026-07-03 14:54:44 +0200

    Upgrade dependencies

--- src/app.d.ts
+++ src/app.d.ts
@@ -8,6 +8,12 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+
+	// Fontsource packages resolve to plain CSS files and have no type declarations
+	// https://fontsource.org/docs/getting-started/faq
+	declare module '*.css';
+	declare module '@fontsource/*' {}
+	declare module '@fontsource-variable/*' {}
 }
 
 export {};
